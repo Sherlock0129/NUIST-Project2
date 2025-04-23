bfe70f4d4da317be651a160856c29c7d723dd74f
夏洛特·福尔马林 <2025352914@qq.com>
2025-04-23 09:31:26 +0800

    Finish SoilSensor.py

---
+++ SoilSensor.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+# GPIO SETUP
+channel = 4
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN)
+
+def callback(channel):
+    if GPIO.input(channel):
+        print("No Water Detected! Please water your plant.")
+    else:
+        print("Water Detected! Water NOT needed.")
+
+# Detect changes in the state of GPIO pins.
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
+GPIO.add_event_callback(channel, callback)
+
+# Infinite loop to keep the program running.
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
